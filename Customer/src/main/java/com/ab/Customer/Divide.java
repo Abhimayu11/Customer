6d3d1370d289a6882d7ed5e528df055879b9a2ec
Abhi <pagade7@gmail.com>
2025-08-30 12:56:28 +0530

    Divide changes

---
+++ Customer/src/main/java/com/ab/Customer/Divide.java
@@ -0,0 +1,10 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+      int a=10;
+	  int b=20;
+	  System.out.println("Divide:"+(b/a));
+    }
+}
